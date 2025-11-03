c6db21d5677bc1c50798112e4c4646710750e201
Cappuccino1784 <ducanhminh0802@gmail.com>
2025-11-03 18:09:29 +0700

    added home, dashboard, log, login, signup pages, WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,47 @@
-function App() {
-  return (
-    <>
-      <div className="bg-red-500">Hello world</div>
-    </>
-  )
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default App
+// Importing pages
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Logs from "./pages/Logs";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route
+						path="/dashboard"
+						element={
+							//<ProtectedRoute>
+							<Dashboard />
+							//</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/logs"
+						element={
+							//<ProtectedRoute>
+							<Logs />
+							//</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/home"
+						element={
+							//<ProtectedRoute>
+							<Home />
+							//</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
+}
