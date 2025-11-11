c71ef2d3f8ecd3659d98939bec21ac1dbc760128
Fong <phamthanhphong160607@gmail.com>
2025-11-11 10:21:51 +0700

    Cookie for signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -44,6 +44,18 @@ export const signup = async (req, res) => {
 			email,
 			password: hashedPassword,
 		});
+
+		const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+			expiresIn: "30d",
+		});
+
+		res.cookie("token", token, {
+			httpOnly: true,
+			secure: true,
+			sameSite: "none",
+			path: "/",
+			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+		});
 		return res.status(201).json({
 			message: "User registered successfully.",
 			user: {
