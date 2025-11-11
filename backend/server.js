1dd8601a106935c7ac8928dd870541d9a7e07184
Fong <phamthanhphong160607@gmail.com>
2025-11-11 10:20:57 +0700

    CORS fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express()
 const __dirname = path.resolve()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://gdg-eunoia.onrender.com'],
     credentials: true,
 }));
 
